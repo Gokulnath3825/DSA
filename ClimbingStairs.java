6ae2191b8e168470343540c8b9299f0c969ce7b0
Gokulnath <gokulj172@gmail.com>
2026-07-28 23:29:30 +0530

    Add files via upload

---
+++ ClimbingStairs.java
@@ -0,0 +1,19 @@
+public class ClimbingStairs {
+
+    public static int count(int stairs) {
+
+        if (stairs == 1)
+            return 1;
+
+        if (stairs == 2)
+            return 2;
+
+        return count(stairs - 1) + count(stairs - 2);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(count(7));
+
+    }
+}
