733478c89b5abb8828b962fcefce1779a27e8e7c
Gokulnath <gokulj172@gmail.com>
2026-09-01 23:42:26 +0530

    Update HouseRobber.java

--- HouseRobber.java
+++ HouseRobber.java
@@ -5,19 +5,14 @@ public class HouseRobber {
 
         if(n==1) return ar[0];
 
-        int p2 = ar[0], p1 = Math.max(ar[0],ar[1]);
-
+        int p2 = ar[0],p1 = Math.max(ar[0],ar[1]);
         int c = 0;
-
         for(int i=2;i<n;i++){
             c = Math.max(p1, ar[i]+p2);
             p2 = p1; p1 = c;
         }
         return p1;
-
-
     } 
-
     public static void main(String[] args) {
 
         int ar [] ={1,2,3,1};
