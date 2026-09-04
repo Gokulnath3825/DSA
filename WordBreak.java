b5ee81ca67c1293a3e0f25a32e43a47f850bb6df
Gokulnath <gokulj172@gmail.com>
2026-09-04 23:06:07 +0530

    Update WordBreak.java

--- WordBreak.java
+++ WordBreak.java
@@ -2,24 +2,16 @@ import java.util.*;
 
 public class WordBreak {
 
-    public static boolean wordBreak(String s, List<String> wordDict) {
+    public static boolean wordBreak(String s, List<String> wordDict){
 
         Set<String> set = new HashSet<>(wordDict);
-
         int n = s.length();
-
         boolean[] dp = new boolean[n + 1];
-
         dp[0] = true;
-
         for (int i = 1; i <= n; i++) {
-
             for (int j = 0; j < i; j++) {
-
                 if (dp[j]) {
-
                     String sub = s.substring(j, i);
-
                     if (set.contains(sub)) {
                         dp[i] = true;
                         break;
@@ -27,16 +19,11 @@ public class WordBreak {
                 }
             }
         }
-
         return dp[n];
     }
-
     public static void main(String[] args) {
-
         String s = "leetcode";
-
         List<String> wordDict = Arrays.asList("leet", "code");
-
         System.out.println(wordBreak(s, wordDict));
     }
 }
